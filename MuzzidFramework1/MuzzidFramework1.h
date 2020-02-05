25d49192b6730b11bb91018cda9384ee750acd83
taind <nguyenductai.job@gmail.com>
2020-02-05 11:36:52 +0700

    Initial commit

---
+++ MuzzidFramework1/MuzzidFramework1.h
@@ -0,0 +1,19 @@
+//
+//  MuzzidFramework1.h
+//  MuzzidFramework1
+//
+//  Created by Tai Nguyen on 2/5/20.
+//  Copyright © 2020 Tai Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MuzzidFramework1.
+FOUNDATION_EXPORT double MuzzidFramework1VersionNumber;
+
+//! Project version string for MuzzidFramework1.
+FOUNDATION_EXPORT const unsigned char MuzzidFramework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuzzidFramework1/PublicHeader.h>
+
+
